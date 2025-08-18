d677afa226765f9e91af68163aef532828545778
Zhang Zhen <zhangzhen@cpl.ac.cn>
2025-08-18 14:21:41 +0800

    update the github link to the new bulk foodie pipeline repo

--- src/config/site.ts
+++ src/config/site.ts
@@ -10,7 +10,7 @@ export const siteConfig: SiteConfig = {
     xielab: "https://sunneyxielab.org/",
     foodie: "https://foodie.sunneyxielab.org/",
     twitter: "https://twitter.com/miickasmt",
-    github: "https://github.com/gaolong/foodie",
+    github: "https://github.com/zhangzhen/bulk-foodie-pipeline",
   },
 };
 
